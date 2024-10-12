18df9a54df9fd43fd433bd9ad17b243b8c0882eb
hooni <jhlee909090@gmail.com>
2024-10-12 16:51:27 +0900

    feat: 헤드섹션 완성

--- 35thSeminaaaa/assignment/TossViewcontroller.swift
+++ 35thSeminaaaa/assignment/TossViewcontroller.swift
@@ -6,56 +6,82 @@
 //
 
 import UIKit
+
 import SnapKit
 import Then
 
-class TossViewcontroller: UIViewController {
+class TossViewcontroller
+: UIViewController {
     
     private let scrollView = UIScrollView()
     private let contentView = UIView()
     
-    private let titleLabel = UILabel().then {
-        $0.text = "제목"
-        $0.font = .boldSystemFont(ofSize: 32)
-        $0.numberOfLines = 0
+    private let appIconImageView = UIImageView().then {
+        $0.backgroundColor = .systemBlue
+        $0.layer.cornerRadius = 20
+        $0.image = UIImage(named: "toss")
+        $0.clipsToBounds = true
     }
     
-    private let versionButton = UIButton(type: .system).then {
-        $0.setTitle("버전 기록", for: .normal)
-        $0.titleLabel?.font = .systemFont(ofSize: 18)
+    private let appNameLabel = UILabel().then {
+        $0.text = "토스"
+        $0.font = .boldSystemFont(ofSize: 20)
     }
     
-    private let openButton = UIButton(type: .system).then {
+    private let appDescriptionLabel = UILabel().then {
+        $0.text = "금융이 쉬워진다"
+        $0.font = .systemFont(ofSize: 14)
+        $0.textColor = .gray
+    }
+    
+    private let getButton = UIButton(type: .system).then {
         $0.setTitle("열기", for: .normal)
-        $0.titleLabel?.font = .systemFont(ofSize: 18)
+        $0.backgroundColor = .systemBlue
+        $0.setTitleColor(.white, for: .normal)
+        $0.layer.cornerRadius = 14
     }
     
     private let shareButton = UIButton(type: .system).then {
-        $0.setTitle("공유", for: .normal)
-        $0.titleLabel?.font = .systemFont(ofSize: 18)
+        $0.setImage(UIImage(systemName: "square.and.arrow.up"), for: .normal)
     }
     
-    private let previewImageView = UIImageView().then {
-        $0.contentMode = .scaleAspectFit
-        $0.backgroundColor = .lightGray
-    }
-    
+    private let ratingView = UIView()
     private let ratingLabel = UILabel().then {
-        $0.text = "평가 및 리뷰"
-        $0.font = .boldSystemFont(ofSize: 24)
+        $0.text = "4.4"
+        $0.font = .boldSystemFont(ofSize: 28)
     }
     
-    private let showAllReviewsButton = UIButton(type: .system).then {
-        $0.setTitle("모두 보기", for: .normal)
-        $0.titleLabel?.font = .systemFont(ofSize: 18)
+    private let ratingStarsLabel = UILabel().then {
+        $0.text = "★★★★☆"
+        $0.font = .systemFont(ofSize: 10)
     }
     
-    private let writeReviewButton = UIButton(type: .system).then {
-        $0.setTitle("리뷰 작성", for: .normal)
-        $0.backgroundColor = .systemBlue
-        $0.setTitleColor(.white, for: .normal)
-        $0.titleLabel?.font = .boldSystemFont(ofSize: 20)
-        $0.layer.cornerRadius = 12
+    private let ratingCountLabel = UILabel().then {
+        $0.text = "8.4만개의 평가"
+        $0.font = .systemFont(ofSize: 12)
+        $0.textColor = .gray
+    }
+    
+    private let ageRatingLabel = UILabel().then {
+        $0.text = "4+"
+        $0.font = .boldSystemFont(ofSize: 28)
+    }
+    
+    private let ageRatingDescriptionLabel = UILabel().then {
+        $0.text = "연령"
+        $0.font = .systemFont(ofSize: 12)
+        $0.textColor = .gray
+    }
+    
+    private let developerLabel = UILabel().then {
+        $0.text = "개발자"
+        $0.font = .boldSystemFont(ofSize: 16)
+    }
+    
+    private let developerNameLabel = UILabel().then {
+        $0.text = "㈜비바리퍼블리카"
+        $0.font = .systemFont(ofSize: 14)
+        $0.textColor = .systemBlue
     }
     
     override func viewDidLoad() {
@@ -64,15 +90,18 @@ class TossViewcontroller: UIViewController {
     }
     
     private func setupUI() {
-        view.backgroundColor = .white
+        view.backgroundColor = .systemBackground
         
         view.addSubview(scrollView)
         scrollView.addSubview(contentView)
         
-        [titleLabel, versionButton, openButton, shareButton, previewImageView, ratingLabel, showAllReviewsButton, writeReviewButton].forEach { contentView.addSubview($0) }
+        [appIconImageView, appNameLabel, appDescriptionLabel, getButton, shareButton,
+         ratingView, ageRatingLabel, ageRatingDescriptionLabel,
+         developerLabel, developerNameLabel].forEach { contentView.addSubview($0) }
+        
+        [ratingLabel, ratingStarsLabel, ratingCountLabel].forEach { ratingView.addSubview($0) }
         
         setupConstraints()
-        setupActions()
     }
     
     private func setupConstraints() {
@@ -85,71 +114,75 @@ class TossViewcontroller: UIViewController {
             $0.width.equalTo(scrollView)
         }
         
-        titleLabel.snp.makeConstraints {
-            $0.top.equalTo(contentView).offset(40)
-            $0.leading.trailing.equalTo(contentView).inset(20)
+        appIconImageView.snp.makeConstraints {
+            $0.top.leading.equalTo(contentView).offset(20)
+            $0.width.height.equalTo(120)
         }
         
-        versionButton.snp.makeConstraints {
-            $0.top.equalTo(titleLabel.snp.bottom).offset(40)
-            $0.leading.equalTo(contentView).offset(20)
-            $0.height.equalTo(44)
+        appNameLabel.snp.makeConstraints {
+            $0.top.equalTo(appIconImageView)
+            $0.leading.equalTo(appIconImageView.snp.trailing).offset(20)
+            $0.trailing.lessThanOrEqualTo(contentView).offset(-20)
         }
         
-        openButton.snp.makeConstraints {
-            $0.top.equalTo(versionButton.snp.bottom).offset(30)
-            $0.leading.equalTo(contentView).offset(20)
-            $0.height.equalTo(44)
+        appDescriptionLabel.snp.makeConstraints {
+            $0.top.equalTo(appNameLabel.snp.bottom).offset(4)
+            $0.leading.trailing.equalTo(appNameLabel)
+        }
+        
+        getButton.snp.makeConstraints {
+            $0.top.equalTo(appDescriptionLabel.snp.bottom).offset(10)
+            $0.leading.equalTo(appNameLabel)
+            $0.width.equalTo(80)
+            $0.height.equalTo(30)
         }
         
         shareButton.snp.makeConstraints {
-            $0.top.equalTo(openButton.snp.bottom).offset(30)
-            $0.leading.equalTo(contentView).offset(20)
-            $0.height.equalTo(44)
+            $0.centerY.equalTo(getButton)
+            $0.trailing.equalTo(contentView).offset(-20)
+            $0.width.height.equalTo(30)
         }
         
-        previewImageView.snp.makeConstraints {
-            $0.top.equalTo(shareButton.snp.bottom).offset(40)
-            $0.leading.trailing.equalTo(contentView).inset(20)
-            $0.height.equalTo(300)
+        ratingView.snp.makeConstraints {
+            $0.top.equalTo(appIconImageView.snp.bottom).offset(30)
+            $0.leading.equalTo(contentView).offset(20)
+            $0.width.equalTo(100)
         }
         
         ratingLabel.snp.makeConstraints {
-            $0.top.equalTo(previewImageView.snp.bottom).offset(40)
+            $0.top.leading.equalTo(ratingView)
+        }
+        
+        ratingStarsLabel.snp.makeConstraints {
+            $0.top.equalTo(ratingLabel.snp.bottom).offset(4)
+            $0.leading.equalTo(ratingView)
+        }
+        
+        ratingCountLabel.snp.makeConstraints {
+            $0.top.equalTo(ratingStarsLabel.snp.bottom).offset(4)
+            $0.leading.equalTo(ratingView)
+            $0.bottom.equalTo(ratingView)
+        }
+        
+        ageRatingLabel.snp.makeConstraints {
+            $0.top.equalTo(ratingView)
+            $0.leading.equalTo(ratingView.snp.trailing).offset(40)
+        }
+        
+        ageRatingDescriptionLabel.snp.makeConstraints {
+            $0.top.equalTo(ageRatingLabel.snp.bottom).offset(4)
+            $0.leading.equalTo(ageRatingLabel)
+        }
+        
+        developerLabel.snp.makeConstraints {
+            $0.top.equalTo(ratingView.snp.bottom).offset(30)
             $0.leading.equalTo(contentView).offset(20)
         }
         
-        showAllReviewsButton.snp.makeConstraints {
-            $0.centerY.equalTo(ratingLabel)
-            $0.trailing.equalTo(contentView).offset(-20)
+        developerNameLabel.snp.makeConstraints {
+            $0.top.equalTo(developerLabel.snp.bottom).offset(4)
+            $0.leading.equalTo(developerLabel)
+            $0.bottom.equalTo(contentView).offset(-20)
         }
-        
-        writeReviewButton.snp.makeConstraints {
-            $0.top.equalTo(ratingLabel.snp.bottom).offset(40)
-            $0.leading.trailing.equalTo(contentView).inset(20)
-            $0.height.equalTo(60)
-            $0.bottom.equalTo(contentView).offset(-40)
-        }
-    }
-    
-    private func setupActions() {
-        versionButton.addTarget(self, action: #selector(showVersionHistory), for: .touchUpInside)
-        showAllReviewsButton.addTarget(self, action: #selector(showAllReviews), for: .touchUpInside)
-    }
-    
-    @objc private func showVersionHistory() {
-        let versionHistoryVC = UIViewController().then {
-            $0.view.backgroundColor = .white
-            $0.title = "버전 기록"
-        }
-        navigationController?.pushViewController(versionHistoryVC, animated: true)
-    }
-    
-    @objc private func showAllReviews() {
-        let allReviewsVC = UIViewController().then {
-            $0.view.backgroundColor = .white
-            $0.title = "모든 리뷰"
-        }
-        navigationController?.pushViewController(allReviewsVC, animated: true)
     }
 }
