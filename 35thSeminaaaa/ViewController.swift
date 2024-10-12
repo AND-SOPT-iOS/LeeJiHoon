d2aa9b40ffc2508110619adb05487ad282b179a0
hooni <jhlee909090@gmail.com>
2024-10-12 16:28:29 +0900

    chore: git add

---
+++ 35thSeminaaaa/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  35thSeminaaaa
+//
+//  Created by 이지훈 on 10/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
