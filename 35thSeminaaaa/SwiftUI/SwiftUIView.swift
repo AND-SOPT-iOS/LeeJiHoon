40c59ac6490c08b9730914c1c70570068db5a338
hooni <jhlee909090@gmail.com>
2024-11-27 13:15:57 +0900

    feat: swiftui view 생성

---
+++ 35thSeminaaaa/SwiftUI/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  35thSeminaaaa
+//
+//  Created by 이지훈 on 11/27/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+//
+#Preview {
+    SwiftUIView()
+}
