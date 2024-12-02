a488b72cf7ad2f76a4459896692c9a01fb3816cf
hooni <jhlee909090@gmail.com>
2024-12-02 15:08:44 +0900

    feat: swiftui 기본세팅

---
+++ 35thSeminaaaa/AndSoptSemina.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  35thSeminaaaa
+//
+//  Created by 이지훈 on 10/12/24.
+//
+
+import SwiftUI
+
+@main
+struct AndSoptSemina: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 35thSeminaaaa/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  35thSeminaaaa
+//
+//  Created by 이지훈 on 12/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentView()
+}
